5718275592ba966d50a72f90e7662fef1a7000b7
unknown <abduseid28@gmail.com>
2024-12-19 17:22:36 +0300

    12-19-2024 update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -32,8 +32,8 @@ export const Footer = () => {
                 <Clock className="w-5 h-5 mr-2 text-teal-600" />
                 Mon-Fri: 8am - 8pm
               </p>
-              <p className="ml-7 text-gray-600">Sat: 9am - 5pm</p>
-              <p className="ml-7 text-gray-600">Sun: Closed</p>
+              <p className="flex items-center text-gray-600"><Clock className="w-5 h-5 mr-2 text-teal-600" /> Sat: 9am - 5pm</p>
+              <p className="flex items-center text-gray-600"><Clock className="w-5 h-5 mr-2 text-teal-600" color='red'/> Sun: Closed</p>
             </div>
           </div>
 
