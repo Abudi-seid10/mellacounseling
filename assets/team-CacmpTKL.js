dae2cc8af2c030a96a707d1978b6f3f9a1b063c4
unknown <abduseid28@gmail.com>
2025-01-18 23:27:37 +0300

    Updates

---
+++ assets/team-CacmpTKL.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-D0FSw78j.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("Award",[["circle",{cx:"12",cy:"8",r:"6",key:"1vp47v"}],["path",{d:"M15.477 12.89 17 22l-5-3-5 3 1.523-9.11",key:"em7aur"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=e("Sparkles",[["path",{d:"m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z",key:"17u4zn"}],["path",{d:"M5 3v4",key:"bklmnn"}],["path",{d:"M19 17v4",key:"iiml17"}],["path",{d:"M3 5h4",key:"nem4j1"}],["path",{d:"M17 19h4",key:"lbex7p"}]]),s=[{id:"abigail-kassaye",name:"Abigail Kassaye",title:"Clinical Psychologist",specializations:["Stress Management","Behavioral Therapy","Cognitive Development"],imageUrl:"https://images.unsplash.com/photo-1559839734-2b71ea197ec2?q=80&w=600&h=800&fit=crop&crop=faces,center"},{id:"hana-tilahun",name:"Hana Tilahun",title:"Licensed Therapist and Counselor",specializations:["Adolescent Therapy","Self-Esteem Building","Career Counseling"],imageUrl:"https://images.unsplash.com/photo-1594824476967-48c8b964273f?q=80&w=600&h=800&fit=crop&crop=faces,center"}];export{t as A,i as S,s as t};
